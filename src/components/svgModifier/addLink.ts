7c89d1f3ebd2ca62e0bda33325ab488ce77fde79
oxeizd <60423962+oxeizd@users.noreply.github.com>
2025-08-21 16:43:05 +1000

    fix link

--- src/components/svgModifier/addLink.ts
+++ src/components/svgModifier/addLink.ts
@@ -13,10 +13,16 @@ export class LinkManager {
       return;
     }
 
+    if (element.hasAttribute('data-has-link')) {
+      return;
+    }
+
     const addlink = document.createElementNS('http://www.w3.org/2000/svg', 'a');
 
     addlink.setAttribute('target', '_blank');
     addlink.setAttribute('href', link);
+
+    element.setAttribute('data-has-link', 'true');
 
     parent.insertBefore(addlink, element);
     addlink.appendChild(element);
